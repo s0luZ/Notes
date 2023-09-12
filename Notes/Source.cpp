e54ffc74a52d3409684dc1c547c74b6adf7d9db6
Isaiah <144374765+s0luZ@users.noreply.github.com>
2023-09-12 01:37:42 -0600

    Add files via upload

---
+++ Notes/Source.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	/*
+	double quarter, dimes, nickels, pennies, totalamount;
+
+	cout << "How many quarters do you have:";
+	cin >> quarter;
+
+	cout << "How many dimes do you have:";
+	cin >> dimes;
+
+	cout << "How many nickels do you have:";
+	cin >> nickels;
+
+	cout << "How many pennies do you have:";
+	cin >> pennies;
+
+	totalamount = (quarter * .25) + (dimes * .10) + (nickels * .05) + (pennies * 1);
+
+	cout << "Your total is =" << totalamount << endl;
+	*/
+
+	//VARIABLES AND BASIC DATA TYPES
+
+			//integer (whole number)
+	int age = 21;
+	int year = 2023;
+	int days = 7;
+
+
+	// double (number including decimal)
+	double price = 10.99;
+	double gpa = 2.5;
+	double temperature = 25.1;
+
+	// single character
+	char grade = 'A';
+	char initial = 'B';
+	char dollarsign = '$';
+
+	//boolean (true or false)
+	bool student = true;
+	bool power = false;
+	bool forSale = true;
+
+	//Strings (sequence of text)
+	string name;
+	string day;
+	string address;
+
+	cout << "What is your Name?: ";
+	cin >> name;
+
+	cout << "What is your address?: ";
+	cin >> address;
+
+	cout << "What day is it today?: ";
+	cin >> day;
+
+
+	//cout << "Hi " << name;
+	//cout << " today is " << day;
+	//cout << " Your address is " << address;
+
+
+
+
+	return 0;
+
+}
